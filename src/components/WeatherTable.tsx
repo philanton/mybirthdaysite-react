86f2bcc0b14d223143e562dcd04a748416492084
philanton <jellyskate1999@gmail.com>
2021-08-30 23:22:51 +0300

    Added opportunity to choose city

--- src/components/WeatherTable.tsx
+++ src/components/WeatherTable.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 
 interface WeatherData {
   dt: number;
@@ -14,7 +14,7 @@ interface WeatherData {
 }
 
 interface WeatherDataFiltered {
-  date: Date;
+  date: number;
   feels_like: number;
   humidity: number;
   pressure: number;
@@ -26,66 +26,81 @@ interface WeatherDataFiltered {
 
 interface WholeWeatherData {
   current: {
-    sunrise: Date;
-    sunset: Date;
+    sunrise: number;
+    sunset: number;
     other: WeatherDataFiltered;
   };
   hourly: WeatherDataFiltered[];
 }
 
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+console.log(process.env);
+
 export default function WeatherTable() {
-  const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
-  const lat = '50.450001';
-  const lon = '30.523333';
-  const URI = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`;
-  const [weatherData, setWeatherData] = useState<WholeWeatherData>({} as WholeWeatherData);
+  const [weatherData, setWeatherData] = useState({} as WholeWeatherData);
+  const [city, setCity] = useState('Kyiv');
 
   useEffect(() => {
-    fetch(URI)
-      .then(response => response.json())
-      .then(data => {
-        const { current, hourly } = data;
-        const weatherData: WholeWeatherData = {
-          current: {
-            sunrise: new Date(current.sunrise * 1000),
-            sunset: new Date(current.sunset * 1000),
-            other: getWeatherDataFiltered(current),
-          },
-          hourly: hourly.slice(0, 24).map((data: WeatherData) => getWeatherDataFiltered(data))
-        };
-        setWeatherData(weatherData);
-      });
-  }, [URI])
-  
-  return Object.keys(weatherData).length === 0 ? null : (
+    const f = async () => {
+      const _weatherData = await getCityWeather(city);
+      setWeatherData(_weatherData);
+    };
+    f();
+  }, [city]);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => setCity(e.target.value);
+
+  return (
     <div className="desert desert-up">
       <div className="content">
         <h2>Weather Forecast</h2>
-        <CurrentWeather
-          sunrise={weatherData.current.sunrise}
-          sunset={weatherData.current.sunset}
-          other={weatherData.current.other}
-        />
-        <div className="overflow-x-scroll border-indigo-400 border-8 rounded-3xl">
-          <table>
-            <thead>
-              <tr>
-                <th className="w-16 sm:w-24">Hour</th>
-                <th className="w-12 sm:w-16"></th>
-                <th className="w-44 sm:w-64">Status</th>
-                <th className="w-52 sm:w-96">Temperature, C</th>
-                <th className="w-32 sm:w-48">Humidity, %</th>
-                <th className="w-36 sm:w-56">Pressure, hPa</th>
-              </tr>
-            </thead>
-            <tbody>
-              {weatherData.hourly.map((data: WeatherDataFiltered) => <WeatherForHour data={data} key={data.date.getHours()}/>)}
-            </tbody>
-          </table>
+        <div className="chunk">
+          <label
+            className="lbl"
+            htmlFor="city"
+            id="city-label"
+          >
+            Your city:
+          </label>
+          <input
+            type="text"
+            id="city"
+            name="city"
+            onChange={handleChange}
+            placeholder="My city"
+            value={city}
+            required
+          />
         </div>
+        {Object.keys(weatherData).length === 0 ? null : (
+          <>
+            <CurrentWeather
+              sunrise={new Date(weatherData.current.sunrise * 1000)}
+              sunset={new Date(weatherData.current.sunset * 1000)}
+              other={weatherData.current.other}
+            />
+            <div className="overflow-x-scroll border-indigo-400 border-8 rounded-3xl">
+              <table>
+                <thead>
+                  <tr>
+                    <th className="w-16 sm:w-24">Hour</th>
+                    <th className="w-12 sm:w-16"></th>
+                    <th className="w-44 sm:w-64">Status</th>
+                    <th className="w-52 sm:w-96">Temperature, C</th>
+                    <th className="w-32 sm:w-48">Humidity, %</th>
+                    <th className="w-36 sm:w-56">Pressure, hPa</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {weatherData.hourly.map((data: WeatherDataFiltered) => <WeatherForHour data={data} key={data.date}/>)}
+                </tbody>
+              </table>
+            </div>
+          </>
+        )}
       </div>
     </div>
-  );
+  )
 }
 
 function CurrentWeather(props: {
@@ -97,7 +112,7 @@ function CurrentWeather(props: {
     <div className="chunk bg-indigo-200 rounded-3xl p-6">
       <div className="flex flex-wrap justify-center items-center m-6">
         <p className="w-30 sm:w-60">
-          {props.other.date.toDateString()}
+          {new Date(props.other.date * 1000).toDateString()}
         </p>
         <em className="emph p-4">
           {props.other.weather_description}
@@ -132,7 +147,7 @@ function WeatherForHour(props: { data: WeatherDataFiltered }) {
   const { data } = props;
   return (
     <tr>
-      <td className="text-right">{data.date.getHours()}</td>
+      <td className="text-right">{new Date(data.date * 1000).getHours()}</td>
       <td>
         <img
           src={data.weather_icon_uri}
@@ -147,9 +162,51 @@ function WeatherForHour(props: { data: WeatherDataFiltered }) {
   );
 }
 
+async function getCityWeather(city: string) {
+  const coords = await getCityCoordinates(typeof city === 'string' ? city : city[0]);
+  if (coords.length === 0) {
+    return {} as WholeWeatherData;
+  }
+
+  const [lat, lon] = coords;
+  const WEATHER_URI = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`;
+
+  const response = await fetch(WEATHER_URI);
+  const data = await response.json();
+
+  if (typeof data.current !== 'undefined') {
+    const { current, hourly } = data;
+    const weatherData: WholeWeatherData = {
+      current: {
+        sunrise: current.sunrise,
+        sunset: current.sunset,
+        other: getWeatherDataFiltered(current),
+      },
+      hourly: hourly.slice(0, 24).map((data: WeatherData) => getWeatherDataFiltered(data))
+    };
+
+    return weatherData;
+  } else {
+    return {} as WholeWeatherData;
+  }
+}
+
+async function getCityCoordinates(city: string) {
+  const CITY_URI = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${API_KEY}`;
+
+  const response = await fetch(CITY_URI);
+  const data = await response.json();
+
+  if (data.length > 0) {
+    return [data[0].lat, data[0].lon];
+  } else {
+    return [];
+  }
+}
+
 function getWeatherDataFiltered(data: WeatherData): WeatherDataFiltered {
   return {
-    date: new Date(data.dt * 1000),
+    date: data.dt,
     feels_like: data.feels_like,
     humidity: data.humidity,
     pressure: data.pressure,
